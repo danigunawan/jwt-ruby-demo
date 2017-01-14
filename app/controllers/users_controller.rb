ba067e0607fa3f8c76163f8ee91c15041dd25c39
enoch232 <enoch232@gmail.com>
2017-01-13 23:15:14 -0700

    add some logic to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,23 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      respond_to do |format|
+        format.json{ @user }
+        format.html{ redirect_to root_path}
+      end
+    else
+      respond_to do |format|
+        format.json{ "Failed to create a user." }
+        format.html{ render :new }
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
