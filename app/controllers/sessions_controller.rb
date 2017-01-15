1b296364468404ba77fb10a14741438be5cfbe6d
enoch232 <enoch232@gmail.com>
2017-01-14 23:51:03 -0700

    add ui

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user.authenticate(params[:password])
+    @user = User.find_by(email: params[:user][:email])
+    if @user.authenticate(params[:user][:password])
       require 'jwt'
-      payload = {data: "test"}
-      puts JWT_SECRET
+      payload = { name: @user.name }
       token = JWT.encode payload, "thisissecretkey", "HS256"
+      session[:current_user_id] = @user.id
       respond_to do |format|
         format.json { render json: { token: token } }
-        # format.html { redirect_to dashboard_path}
+        format.html { redirect_to dashboard_path, notice: "Signed in successfully!" }
       end
     else
       respond_to do |format|
         format.json { render json: { message: "invalid" } }
+        format.html { redirect_to new_session_path, notice: "Wrong credentials!" }
       end
     end
 
@@ -23,11 +24,5 @@ class SessionsController < ApplicationController
 
   def destroy
 
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password)
   end
 end
