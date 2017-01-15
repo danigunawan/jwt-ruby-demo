b0bdf82f576d323a6ed4c37ad186205347f74767
enoch232 <enoch232@gmail.com>
2017-01-15 01:24:59 -0700

    use secrets.yml

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if @user.authenticate(params[:user][:password])
       require 'jwt'
       payload = { name: @user.name }
-      token = JWT.encode payload, "thisissecretkey", "HS256"
+      token = JWT.encode payload, Rails.application.secrets.JWT_SECRET, "HS256"
       session[:current_user_id] = @user.id
       session[:last_login_time] = Time.now
       respond_to do |format|
